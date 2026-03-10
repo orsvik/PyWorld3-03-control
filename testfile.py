43a6970f4cf0c5674c9da42103cfd537381183ab
Linnea Stålberg <lstal@kth.se>
2026-03-10 10:18:18 +0100

    testfile for just standard run

---
+++ testfile.py
@@ -0,0 +1,71 @@
+
+import numpy as np
+from pyworld3 import World3
+from matplotlib import pyplot as plt
+import pandas as pd
+from tqdm import tqdm
+from pyworld3.utils import plot_world_variables
+
+
+max_year = 2100
+world3 = World3(year_max=max_year)
+world3.set_world3_control()
+world3.init_world3_constants()
+world3.init_world3_variables()
+world3.set_world3_table_functions()
+world3.set_world3_delay_functions()
+world3.run_world3(fast=False) # want to be able to set fast=True
+
+
+
+plot_world_variables(
+    world3.time,
+    [world3.al, world3.pal, world3.uil, world3.lfert],
+    ["AL", "PAL", "UIL", "LFERT"],
+    [[0, 25e8], [0, 23e8], [0, 120e6], [0, 800]],
+    figsize=(7, 5),
+    title="World3 control run - Agriculture",
+)
+plt.grid()
+
+
+plot_world_variables(
+    world3.time,
+    [world3.ic, world3.sc],
+    ["IC", "SC"],
+    [[0, 12e12], [0, 50e11]],
+    figsize=(7, 5),
+    title="World3 control run - Capital",
+)
+plt.grid()
+
+plot_world_variables(
+    world3.time,
+    [world3.pp],
+    ["PP"],
+    [[0, 20e8]],
+    figsize=(7, 5),
+    title="World3 control run - Pollution",
+)
+plt.grid()
+
+plot_world_variables(
+    world3.time,
+    [world3.p1, world3.p2, world3.p3, world3.p4],
+    ["P1", "P2", "P3", "P4"],
+    [[0, 3e9], [0, 5e9], [0, 2e9], [0, 2e9]],
+    figsize=(7, 5),
+    title="World3 control run - Population",
+)
+plt.grid()
+
+plot_world_variables(
+    world3.time,
+    [world3.nr],
+    ["NR"],
+    [[0, 1e12]],
+    figsize=(7, 5),
+    title="World3 control run - Resources",
+)
+plt.grid()
+plt.show()
