c55ced86a25989c98f3647d43d3b40d32efee186
Evelina Örsvik <evelina.ohrsvik@gmail.com>
2026-03-13 14:37:45 +0100

    Implement HSDI reward for comparison

--- data_generation.py
+++ data_generation.py
@@ -52,14 +52,60 @@ def J_func(reward):
         J[iterations-k] = reward[iterations-k] + J[iterations-k+1]
     return J
 
+
+# -- REWARD FUNCTIONS DEFINITION ZONE --
+
 def reward_hwi(world):
     # A reward function g being the Human Welfare Index (HWI) as defined in PyWorld3-03
     return world.hwi
 
+def reward_HSDI(world):
+    # HSDI "approximation"
+
+    # le
+    min_le = np.min(world_standard.le) * 0.95
+    max_le = np.max(world_standard.le) * 1.05
+    I_le = (world.le - min_le) / (max_le - min_le)
+    I_le = np.clip(I_le, 0, 1)
+
+    # j/pop
+    ref_jpop = world_standard.j / world_standard.pop
+    min_jpop = np.min(ref_jpop) * 0.95
+    max_jpop = np.max(ref_jpop) * 1.05
+    jpop = world.j/world.pop
+    I_jpop = (jpop - min_jpop) / (max_jpop - min_jpop)
+    I_jpop = np.clip(I_jpop, 0, 1)
+
+    # GDP
+    min_gdp = np.min(world_standard.iopc + world_standard.sopc) * 0.95
+    max_gdp = np.max(world_standard.iopc + world_standard.sopc) * 1.05
+    world_gdp = world.iopc + world.sopc
+    I_gdp = (world_gdp - min_gdp) / (max_gdp - min_gdp)
+    I_gdp = np.clip(I_gdp, 0, 1)
+
+    # Pollution
+    # NOTE: ppol has been replaced with pp
+    min_pp_pop = np.min(world_standard.pp / world_standard.pop) * 0.95
+    max_pp_pop = np.max(world_standard.pp / world_standard.pp) * 1.05
+    pp_pop = world.pp / world.pop
+    I_pp_pop = 1 - ((pp_pop - min_pp_pop) / (max_pp_pop - min_pp_pop))
+    I_pp_pop = np.clip(I_pp_pop, 0, 1)
+
+    # HSDI
+    reward = (I_le * I_jpop * I_gdp * I_pp_pop) ** (1/4)
+    return reward
+
+
+# --
+
+# Select here which reward function to use throughout the rest of the file
+REWARD_FUNC = reward_HSDI
+
+
 if PLOT:
     # Plots and prints for intuition
-    print(reward_hwi(world_standard))
-    plt.plot(reward_hwi(world_standard))
+    print(REWARD_FUNC(world_standard))
+    plt.plot(REWARD_FUNC(world_standard))
     plt.show()
 
 def get_mu_sigma(world, variable):
@@ -159,4 +205,4 @@ def main(chosen_reward):
         df.to_parquet(f"datasets/data_{reward_func_name}.parquet", index=False) # see pandas.DataFrame.to_parquet documentation for why index=False
         print("The data was saved to file.")
 
-main(reward_hwi)
+main(REWARD_FUNC)
