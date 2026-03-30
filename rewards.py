8f7776b4bf4df29a0093822e5c79940a5c339f58
Evelina Örsvik <evelina.ohrsvik@gmail.com>
2026-03-30 21:08:48 +0200

    Run RL for ddiff, 5000 datapoints

--- rewards.py
+++ rewards.py
@@ -27,7 +27,7 @@ def reward_inv_ef(world, k=None):
 def reward_ddiff(world, k=None, hwi_weight=1.0, ef_weight=0.25):
     # Inspired by doughnut economics
     reward = hwi_weight * world.hwi - ef_weight * world.ef
-    reward += 100
+    #reward += 100
     if k is None:
         return reward
     else:
