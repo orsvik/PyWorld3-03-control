3ac8ba1cc810becfae321eda25d892ca3a355a7b
Evelina Örsvik <evelina.ohrsvik@gmail.com>
2026-04-23 18:36:56 +0200

    NN ID 3

---
+++ rewardplots.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def hwi_reward_demo(x, hwi_limit=0.55, hwi_alpha=4.0, hwi_beta=0.1, hwi_h=1.1):
+    # Doughnut economics approach which strongly punishes boundary transgression
+    return hwi_h - np.exp(hwi_alpha * (hwi_limit - x) + hwi_beta)
+
+def ef_reward_demo(x, ef_limit=1.1, ef_alpha=0.5, ef_beta=-0.5, ef_h=0.6):
+    return ef_h - np.exp(ef_alpha * (x - ef_limit) + ef_beta)
+
+hwi_demo_xs = np.linspace(0, 1, 100)
+hwi_demo_ys = hwi_reward_demo(hwi_demo_xs)
+
+plt.plot(hwi_demo_xs, hwi_demo_ys, label="HWI reward")
+
+ef_demo_xs = np.linspace(0, 6.5, 650)
+ef_demo_ys = ef_reward_demo(ef_demo_xs)
+
+plt.plot(ef_demo_xs, ef_demo_ys, label="HEF reward")
+plt.legend()
+plt.show()
